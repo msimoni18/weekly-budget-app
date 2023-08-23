371d12605790d8975ba60da9508b949dbdf22cca
Matthew Simoni <simoni2499@gmail.com>
2023-08-22 21:56:04 -0400

    modified layout of progress bar and added ability to modify weekly budget

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,24 @@ import { Toaster } from 'react-hot-toast'
 import Layout from './components/Layout/Layout'
 import Home from './components/routes/Home/Home'
 import Transactions from './components/routes/Transactions/Transactions'
-import { useAppDispatch, useTransactions } from './hooks/hooks'
-import { updateTransactions, updateWeeklyTotal } from './appSlice'
+import { useAppDispatch, useTransactions, useWeeklyBudget } from './hooks/hooks'
+import {
+  updateTransactions,
+  updateWeeklyTotal,
+  updateWeeklyBudget,
+} from './appSlice'
 import './App.css'
 
 function App() {
   const { transactions, total } = useTransactions()
+  const weeklyBudget = useWeeklyBudget()
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    console.log('App: useEffect')
     dispatch(updateTransactions(transactions))
     dispatch(updateWeeklyTotal(total))
-  }, [dispatch, transactions, total])
+    dispatch(updateWeeklyBudget(weeklyBudget))
+  }, [dispatch, transactions, total, weeklyBudget])
 
   return (
     <>
--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,45 +1,120 @@
+import { useState, useEffect, ChangeEvent } from 'react'
 import cn from 'classnames'
-import { useAppSelector } from '../../hooks/hooks'
+import { toast } from 'react-hot-toast'
+import { FaRegEdit, FaRegSave } from 'react-icons/fa'
+import { useAppSelector, useAppDispatch } from '../../hooks/hooks'
+import { post } from '../../utilities/requests'
+import { updateWeeklyBudget } from '../../appSlice'
+import { WeeklyBudgetResponse } from '../../types/types'
 
 interface ProgressBar {
   week: { firstDay: string; lastDay: string }
-  budget?: number
 }
 
-export default function ProgressBar({ week, budget = 250 }: ProgressBar) {
+function BudgetInput({ ...props }) {
+  return (
+    <input
+      className="w-28 bg-stone-950 rounded-lg border border-stone-400 focus:border-stone-200 focus:outline-none focus:ring-0 pl-2"
+      {...props}
+    />
+  )
+}
+
+export default function ProgressBar({ week }: ProgressBar) {
+  const dispatch = useAppDispatch()
   const total = useAppSelector((state) => state.app.weeklyTotal)
-  const percentage = (total / budget) * 100
-  const remaining = budget - total
+  const weeklyBudget = useAppSelector((state) => state.app.weeklyBudget)
+  const percentage = (total / weeklyBudget.amount) * 100
+  const remaining = weeklyBudget.amount - total
+
+  const [newBudget, setNewBudget] = useState(0)
+  const [isEditing, setIsEditing] = useState(false)
+
+  useEffect(() => {
+    setNewBudget(weeklyBudget.amount)
+  }, [weeklyBudget.amount])
+
+  const handleResponse = (response: WeeklyBudgetResponse) => {
+    if (response.status === 500) {
+      toast.error(response.message)
+    } else {
+      dispatch(updateWeeklyBudget(response.weekly_budget))
+    }
+  }
+
+  const handleStartEdit = () => {
+    setIsEditing(true)
+    setNewBudget(weeklyBudget.amount)
+  }
+
+  const handleStopEdit = () => {
+    setIsEditing(false)
+    post(
+      JSON.stringify({
+        id: weeklyBudget.id,
+        amount: newBudget,
+      }),
+      'get-weekly-budget',
+      (response) => handleResponse(response as WeeklyBudgetResponse),
+      (error) => console.error(error),
+    )
+  }
 
   return (
     <>
       <div className="text-center mb-2.5">
-        <h1>Progress for</h1>
-        <h1>
+        <p>
           <span className="text-sky-500">{week['firstDay']}</span> to{' '}
           <span className="text-sky-500">{week['lastDay']}</span>
-        </h1>
+        </p>
       </div>
       <div className="flex justify-between">
-        <p>
-          Remaining: $<span>{remaining.toLocaleString()}</span>
-        </p>
-        <p>
-          Total: $<span>{total.toLocaleString()}</span>
-        </p>
+        <p>Weekly Progress</p>
+        <p>${remaining.toLocaleString()} left</p>
       </div>
-      <div className="w-full h-6 border rounded-xl overflow-hidden">
+      <div className="w-full h-6 border rounded-xl overflow-hidden my-1">
         <div
           className={cn(
             'h-full',
-            total <= budget ? 'bg-sky-500' : 'bg-red-500',
+            total <= weeklyBudget.amount ? 'bg-sky-500' : 'bg-red-500',
           )}
           style={{ width: `${percentage}%` }}
         ></div>
       </div>
-      <div className="flex justify-between">
-        <p>$0</p>
-        <p>${budget.toLocaleString()}</p>
+      <div className="flex justify-between align-items">
+        <p>
+          ${total.toLocaleString()} of $
+          {isEditing ? (
+            <BudgetInput
+              type="number"
+              value={newBudget}
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setNewBudget(parseFloat(event.target.value))
+              }
+            />
+          ) : (
+            weeklyBudget.amount?.toLocaleString()
+          )}
+        </p>
+        {!isEditing ? (
+          <button
+            type="button"
+            className="flex items-center gap-1"
+            onClick={handleStartEdit}
+          >
+            <FaRegEdit />
+            Edit
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="flex items-center gap-1"
+            onClick={handleStopEdit}
+          >
+            <FaRegSave />
+            Save
+          </button>
+        )}
       </div>
     </>
   )
